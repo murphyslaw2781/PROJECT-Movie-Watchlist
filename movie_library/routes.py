6a9971f6f02a51ea0c561dbc41ba5d1d57d6d0bf
murphyslaw2781 <tylerpeterson.a@gmail.com>
2022-05-18 21:42:23 -0500

    Dark Mode

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    session,
+    request,
+)
 
 
 pages = Blueprint(
@@ -12,3 +18,12 @@ def index():
         "index.html",
         title="Movies Watchlist",
     )
+@pages.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))

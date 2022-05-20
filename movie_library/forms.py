29720d7fabc527f1c9fc7207a6977939ffaa057d
murphyslaw2781 <tylerpeterson.a@gmail.com>
2022-05-19 21:57:25 -0500

    Movie details | edit form

--- movie_library/forms.py
+++ movie_library/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField
+from wtforms import (
+    IntegerField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+    URLField,
+)
 
-from wtforms.validators import InputRequired, NumberRange
+from wtforms.validators import (
+    InputRequired,
+    NumberRange,
+)
 
 
 class MovieForm(FlaskForm):
@@ -12,8 +21,33 @@ class MovieForm(FlaskForm):
         "Year",
         validators=[
             InputRequired(),
-            NumberRange(min=1878, max=2022, message="Enter YYYY"),
-        ]
+            NumberRange(min=1878, message="Please enter a year in the format YYYY."),
+        ],
     )
 
     submit = SubmitField("Add Movie")
+
+
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        # checks valuelist contains at least 1 element, and the first element isn't falsy (i.e. empty string)
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendedMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringListField("Series")
+    tags = StringListField("Tags")
+    description = TextAreaField("Description")
+    video_link = URLField("Video link")
+
+    submit = SubmitField("Submit")
--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 from dataclasses import asdict
 
 from flask import (
@@ -10,7 +11,7 @@ from flask import (
     url_for,
     request,
 )
-from movie_library.forms import MovieForm
+from movie_library.forms import MovieForm, ExtendedMovieForm
 from movie_library.models import Movie
 
 
@@ -21,9 +22,16 @@ pages = Blueprint(
 
 @pages.route("/")
 def index():
+    # Lookup movies and store in movie_data
+    # Then unpack movies and create object using Movie class
+    # Creates a list of movies you can pass into render template
+    movie_data = current_app.db.movie.find({})
+    movies = [Movie(**movie) for movie in movie_data]
+
     return render_template(
         "index.html",
         title="Movies Watchlist",
+        movies_data=movies,
     )
 
 
@@ -46,6 +54,46 @@ def add_movie():
     return render_template(
         "new_movie.html", title="Movies Watchlist - Add Movie", form=form
     )
+
+
+@pages.get("/movie/<string:_id>")
+def movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    return render_template("movie_details.html", movie=movie)
+
+@pages.route("/edit/<string:_id>", methods=["GET", "POST"])
+def edit_movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    form = ExtendedMovieForm(obj=movie)
+    if form.validate_on_submit():
+        movie.title = form.title.data
+        movie.director = form.director.data
+        movie.year = form.year.data
+        movie.cast = form.cast.data
+        movie.series = form.series.data
+        movie.tags = form.tags.data
+        movie.description = form.description.data
+        movie.video_link = form.video_link.data
+
+        current_app.db.movie.update_one({"_id": movie._id}, {"$set": asdict(movie)})
+        return redirect(url_for(".movie", _id=movie._id))
+    return render_template("movie_form.html", movie=movie, form=form)
+
+@pages.get("/movie/<string:_id>/watch")
+def watch_today(_id):
+    current_app.db.movie.update_one(
+        {"_id": _id}, {"$set": {"last_watched": datetime.datetime.today()}}
+    )
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.get("/movie/<string:_id>/rate")
+def rate_movie(_id):
+    rating = int(request.args.get("rating"))
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
+
+    return redirect(url_for(".movie", _id=_id))
 
 
 @pages.get("/toggle-theme")
